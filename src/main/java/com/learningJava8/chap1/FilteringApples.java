cf1891e765649366d13a2ba77135007389c47570
longben2017 <479501260@qq.com>
2017-05-27 00:11:10 +0800

    continue java7 example

--- src/main/java/com/learningJava8/chap1/FilteringApples.java
+++ src/main/java/com/learningJava8/chap1/FilteringApples.java
@@ -1,9 +1,15 @@
 package com.learningJava8.chap1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FilteringApples {
+	List<Apple> applesList = Arrays.asList(new Apple(80,"green"),
+										   new Apple(160,"red"),
+										   new Apple(50,"red"));
+	
+	
 	//1.Someone want to filter green apples in a list.
 	public static List<Apple> filterGreenApples(List<Apple> applesList){
 		List<Apple> result = new ArrayList<Apple>();
@@ -27,6 +33,10 @@ public class FilteringApples {
 		return result;
 	}
 	
+	//Now you can run it by this way.
+	List<Apple> greenApples = filterApplesByColor(applesList, "green");
+	List<Apple> redApples = filterApplesByColor(applesList, "red");
+	
 	//3.Someone want to filter heavy or light apples in a list.
 	//So you need to create a new method to make the weight become a parameter.
 	public static List<Apple> filterApplesByWeight(List<Apple> applesList,int weight){
@@ -39,10 +49,24 @@ public class FilteringApples {
 		return result;
 	}
 	
+	//4.But the above two methods lead to too much code.You will try to combine these two methods.
+	//It needs a sign to distinguish which method is called.
+	public static List<Apple> filterApples(List<Apple> applesList,String color,int weight,boolean flag){
+		List<Apple> result = new ArrayList<Apple>();
+		for(Apple apple : applesList){
+			if(flag && apple.getColor().equals(color) || 
+					!flag && apple.getWeight() > weight ){
+				result.add(apple);
+			}
+		}
+		return result;
+	}
 	
-	
-	
-	
+	//When you want to filter green apples,you will set true.Or you want to filter heavy apples,you will set false.
+	//But the client code is bad.No one knows true or false except you.
+	//If someone adds another condition,it's not work.
+	List<Apple> greenApples2 = filterApples(applesList, "green", 0, true);
+	List<Apple> heavyApples = filterApples(applesList, "", 150, false);
 	
 	
 	
