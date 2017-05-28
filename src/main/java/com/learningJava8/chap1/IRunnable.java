e6eaa0ca8ee52a12a0386d6bfd26a1a10cad0428
longben2017 <479501260@qq.com>
2017-05-28 14:27:03 +0800

    try to use lambda to run thread

--- src/main/java/com/learningJava8/chap1/IRunnable.java
+++
@@ -1,5 +0,0 @@
-package com.learningJava8.chap1;
-
-public interface IRunnable {
-	public void run();
-}
--- src/main/java/com/learningJava8/chap1/RunnableByLambda.java
+++ src/main/java/com/learningJava8/chap1/RunnableByLambda.java
@@ -1,10 +1,15 @@
 package com.learningJava8.chap1;
 
-public class RunnableByLambda implements IRunnable{
-
-	@Override
-	public void run() {
+public class RunnableByLambda{
+	public static void main(String[] args) {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Hello world");
+			}
+		});
 		
+		Thread thread2 = new Thread(()-> System.out.println("Hello world!!!"));	
+		thread2.start();
 	}
-	
 }
